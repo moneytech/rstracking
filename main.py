3c130f0d57a03db6bb7cb52db36b28f1d6a39bfa
Yoonsik Park <park.yoonsik@icloud.com>
2019-05-10 17:19:35 -0500

    migrated to Flask

--- main.py
+++ main.py
@@ -1,78 +1,96 @@
 import sqlite3
 import os
-import re
+import io
+from flask import Flask, jsonify, request, send_file, abort
+from flask_cors import CORS
 import time
 import uuid
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class TrackingRequestHandler(BaseHTTPRequestHandler):
-  # GET
-    def do_GET(self):
-        # Send response status code
-        split_path = self.path.split('/')
-        if split_path[1] == 'api':
-            if split_path[2] != secret_key:
-                self.send_response(403)
-                self.end_headers()
-                self.wfile.write(bytes("", "UTF8"))
-                return
-            self.send_response(200)
-            self.send_header('Content-type','text/plain')
-            self.end_headers()
-            if self.path.endswith('history'):
-                for row in c.execute(f'SELECT * FROM history ORDER BY access_time'):
-                    self.wfile.write(bytes(str(row)[1:-1] + '\n', "UTF8"))
-            elif self.path.endswith('trackers'):
-                for row in c.execute(f'SELECT * FROM trackers ORDER BY hit_count'):
-                    self.wfile.write(bytes(str(row)[1:-1] + '\n', "UTF8"))
-            elif len(split_path) > 3:
-                tracker_id = get_new_tracker(None, split_path[-1])
-                self.wfile.write(bytes(tracker_id + '\n', "UTF8"))
-            conn.commit()
-            return
-        try:
-            uuid.UUID(split_path[-1])
-            self.send_response(200)
-            self.send_header('Content-type','image/gif')
-            self.send_header('Cache-Control', 'no-cache')
-            self.end_headers()
-            
-            ip_address = self.address_string()
-            user_agent = "NULL"
-            accept_language = "NULL"
-            if "User-Agent" in self.headers:
-                user_agent = self.headers.get("User-Agent")
-            if "Accept-Language" in self.headers:
-                accept_language = self.headers.get("Accept-Language")            
-            receive_request(split_path[-1].lower(), ip_address, user_agent, accept_language)
-            self.wfile.write(ret_gif)
-        except (ValueError):
-            self.send_response(404)
-            self.end_headers()
-            self.wfile.write(bytes("", "UTF8"))
-        # Send headers
-        return
+app = Flask(__name__, static_url_path='', static_folder='static')
+CORS(app)
+
+gif_file = open("transparent.gif", "rb")
+ret_gif = gif_file.read()
+gif_file.close()
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+## GET route for tracking
+@app.route('/trackers/<tracker_id>.gif')
+def hit(tracker_id):
+    try:
+        uuid.UUID(tracker_id)
+    except (ValueError):
+        abort(404)
+    ip_address = request.remote_addr
+    user_agent = request.headers.get("User-Agent")
+    accept_language = request.headers.get("Accept-Language")
+    receive_request(tracker_id.lower(), ip_address, user_agent, accept_language)
+    return send_file(io.BytesIO(ret_gif), mimetype='image/gif')
+
+## GET route for history
+@app.route('/api/<url_key>/history')
+def get_history(url_key):
+    if url_key != secret_key:
+        abort(403)
+    with sqlite3.connect(sqlite_file) as conn:
+        c = conn.cursor()
+        ret_list = []
+        for row in c.execute(f'SELECT * FROM history ORDER BY access_time'):
+            ret_list.append(row)
+        return jsonify(ret_list)
+
+## GET route for adding a tracker
+@app.route('/api/<url_key>/add_tracker')
+def add_tracker(url_key):
+    if url_key != secret_key:
+        abort(403)
+    tracker_id = get_new_tracker(None, "random")
+    return jsonify(tracker_id)
+
+## GET route for list of trackers
+@app.route('/api/<url_key>/trackers')
+def get_trackers(url_key):
+    if url_key != secret_key:
+        abort(403)
+    with sqlite3.connect(sqlite_file) as conn:
+        c = conn.cursor()
+        ret_list = []
+        for row in c.execute(f'SELECT * FROM trackers ORDER BY hit_count'):
+            ret_list.append(row)
+        return jsonify(ret_list)
 
 def get_new_tracker(grouping, description):
-    new_uuid = str(uuid.uuid4())
-    try:
-        c.execute(f"INSERT INTO trackers (uuid, grouping, description, hit_count) VALUES (?, ?, ?, 0)",
-                                    (new_uuid, grouping, description))
-        return new_uuid
-    except sqlite3.IntegrityError:
-        return "failure"
+    with sqlite3.connect(sqlite_file) as conn:
+        c = conn.cursor()
+        new_uuid = str(uuid.uuid4())
+        try:
+            c.execute(f"INSERT INTO trackers (uuid, grouping, description, hit_count) VALUES (?, ?, ?, 0)",
+                                        (new_uuid, grouping, description))
+            conn.commit()
+            return new_uuid
+        except sqlite3.IntegrityError:
+            return "failure"
 
 def receive_request(request_id, ip_address, user_agent, accept_language):
-    c.execute(f"INSERT INTO history"
-              f"(tracker_id, ip_address, user_agent, accept_language, access_time)"
-              f" VALUES (?, ?, ?, ?, ?)",
-                (request_id, ip_address, user_agent, accept_language, int(time.time())))
-    try:
-        c.execute(f"INSERT INTO trackers (uuid, hit_count) VALUES (?, 1)", (request_id,))
-    except sqlite3.IntegrityError:
-        c.execute(f"UPDATE trackers SET hit_count = hit_count + 1 WHERE uuid = ?", (request_id,))
-    conn.commit()
-    print("received request")
+    with sqlite3.connect(sqlite_file) as conn:
+        c = conn.cursor()
+        c.execute(f"INSERT INTO history"
+                f"(tracker_id, ip_address, user_agent, accept_language, access_time)"
+                f" VALUES (?, ?, ?, ?, ?)",
+                    (request_id, ip_address, user_agent, accept_language, int(time.time())))
+        try:
+            c.execute(f"INSERT INTO trackers (uuid, hit_count) VALUES (?, 1)", (request_id,))
+        except sqlite3.IntegrityError:
+            c.execute(f"UPDATE trackers SET hit_count = hit_count + 1 WHERE uuid = ?", (request_id,))
+        conn.commit()
+        print("received request")
 
 def initialize_database():
     c.execute(f'CREATE TABLE trackers ('
@@ -93,36 +111,19 @@ def initialize_database():
               f')')
     conn.commit()
 
-def start_server():
-    print('starting server...') 
-    # Server settings
-    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('', 8080)
-    httpd = HTTPServer(server_address, TrackingRequestHandler)
-    print('running server...')
-    httpd.serve_forever()
-
-gif_file = open("transparent.gif", "rb")
-ret_gif = gif_file.read()
-gif_file.close()
-history_table = 'history'  # name of the history table
-trackers_table = 'trackers'  # name of the trackers table
 sqlite_file = 'tracking.db'    # name of the sqlite database file
 
-try:
-    if not os.path.isfile(sqlite_file):
-        conn = sqlite3.connect(sqlite_file)
-        c = conn.cursor()
-        initialize_database()
-    else:
-        conn = sqlite3.connect(sqlite_file)
-        c = conn.cursor()
-    secret_key = "70924a89154d5a7d8d60393a0880828e795bdb17b2cba43b"
-    if secret_key == "":
-        secret_key = os.urandom(24).hex()
-    print(f"Your secret URL: /api/{secret_key}")
-    start_server()
+if not os.path.isfile(sqlite_file):
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
+    initialize_database()
     conn.close()
+secret_key = "70924a89154d5a7d8d60393a0880828e795bdb17b2cba43b"
+if secret_key == "":
+    secret_key = os.urandom(24).hex()
+print(f"Your secret URL: /api/{secret_key}")
 
-except (KeyboardInterrupt, sqlite3.Error):
-    conn.close()
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True, port=os.environ.get('PORT', 8080))
+    
